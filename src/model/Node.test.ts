6e77636e86af98678a74afd4e9b85217230e7c8c
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-09-15 12:57:48 +0200

    Remove key attribute

--- src/model/Node.test.ts
+++ src/model/Node.test.ts
@@ -1,17 +1,15 @@
 import { Node } from "./Node";
 
 test("Node instantiation", () => {
-  let node = new Node(0);
+  let node = new Node();
   expect(node).toEqual({
-    key: 0,
     left: null,
     right: null,
     data: null,
   });
 
-  const node2 = new Node(0, { test: 42 });
+  const node2 = new Node({ test: 42 });
   expect(node2).toEqual({
-    key: 0,
     left: null,
     right: null,
     data: { test: 42 },
@@ -19,10 +17,10 @@ test("Node instantiation", () => {
 });
 
 test("Depth first search", () => {
-  const root = new Node(0, { name: "root" });
-  const node1 = new Node(1, { name: "node 1" });
-  const node2 = new Node(2, { name: "node 2" });
-  const node3 = new Node(3, { name: "node 3" });
+  const root = new Node({ name: "root" });
+  const node1 = new Node({ name: "node 1" });
+  const node2 = new Node({ name: "node 2" });
+  const node3 = new Node({ name: "node 3" });
 
   root.left = node1;
   root.right = node2;
@@ -31,9 +29,9 @@ test("Depth first search", () => {
   const depthArray = [...root.dfs()];
 
   expect(depthArray).toEqual([
-    { key: 0, left: node1, right: node2, data: { name: "root" } },
-    { key: 1, left: null, right: null, data: { name: "node 1" } },
-    { key: 2, left: null, right: node3, data: { name: "node 2" } },
-    { key: 3, left: null, right: null, data: { name: "node 3" } },
+    { left: node1, right: node2, data: { name: "root" } },
+    { left: null, right: null, data: { name: "node 1" } },
+    { left: null, right: node3, data: { name: "node 2" } },
+    { left: null, right: null, data: { name: "node 3" } },
   ]);
 });
--- src/model/Node.ts
+++ src/model/Node.ts
@@ -1,11 +1,9 @@
 export class Node<T> {
-  key: number;
   data: T | null;
   left: Node<T> | null;
   right: Node<T> | null;
 
-  constructor(key: number, data?: T) {
-    this.key = key;
+  constructor(data?: T) {
     this.data = data ?? null;
     this.left = null;
     this.right = null;

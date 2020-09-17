58a349bd5e07bbe85cc99cb0bed9392227dee597
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-09-17 18:19:33 +0200

    Steal button styling from bootstrap

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { IconType } from "react-icons";
 import "./Button.css";
 
-const Button: React.FC = (props) => null;
+type ButtonProps = {
+  Icon?: IconType;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+};
+
+const Button: React.FC<ButtonProps> = ({ children, onClick, Icon }) => (
+  <button className="Button" onClick={onClick}>
+    <div className="button-content">
+      {Icon && <Icon className="button-icon" />}
+      {children}
+    </div>
+  </button>
+);
 
 export default Button;

6867435a95e1a1a374d6bbddbc0de0cd250cc15d
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-09-17 18:41:02 +0200

    Handle item input

--- src/components/ArrayInput/ArrayInput.tsx
+++ src/components/ArrayInput/ArrayInput.tsx
@@ -1,22 +1,38 @@
 import React from "react";
 import "./ArrayInput.css";
+import useArrayInputState from "./useArrayInputState";
+import Button from "../Button";
 
 type ArrayInputProps<T> = {
   currentList: T[];
   onInsert: () => void;
 };
 
-const ArrayInput = <T,>({ currentList }: ArrayInputProps<T>) => (
-  <div className="ArrayInput">
-    {currentList.map((item, index) => (
-      <div key={index} className="array-item">
-        {item}
+const ArrayInput = <T,>({ currentList, onInsert }: ArrayInputProps<T>) => {
+  const [state, dispatch] = useArrayInputState();
+  return (
+    <div className="ArrayInput">
+      {currentList.map((item, index) => (
+        <div key={index} className="array-item">
+          {item}
+        </div>
+      ))}
+      <div className="array-item item-input-wrapper">
+        <input
+          className="item-input"
+          type="text"
+          size={
+            state.inputValue !== null ? state.inputValue.toString().length : 1
+          }
+          value={state.inputValue ?? ""}
+          onChange={(e) => dispatch({ type: "EDIT", value: e.target.value })}
+        />
       </div>
-    ))}
-    <div className="array-item item-input-wrapper">
-      <input className="item-input" type="text" placeholder="nombre" size={1} />
+      <Button className="add-number-button" onClick={onInsert}>
+        Add
+      </Button>
     </div>
-  </div>
-);
+  );
+};
 
 export default ArrayInput;

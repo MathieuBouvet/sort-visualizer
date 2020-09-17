fc6d91fa25c985ed9ccd666d210577a600f2bee2
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-09-17 19:00:55 +0200

    Test for updating the item value

--- src/components/ArrayInput/ArrayInput.test.tsx
+++ src/components/ArrayInput/ArrayInput.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import ArrayInput from "./ArrayInput";
 
 test("render the given list", () => {
@@ -7,4 +7,13 @@ test("render the given list", () => {
     <ArrayInput currentList={[42, 21, 56, -45]} onInsert={() => {}} />
   );
   expect(getAllByText(/(42)|(21)|(56)|(-45)/i).length).toBe(4);
+});
+
+test("updating the item value ", () => {
+  const { getByRole, getByDisplayValue } = render(
+    <ArrayInput currentList={[]} onInsert={() => null} />
+  );
+  const itemInput = getByRole("textbox");
+  fireEvent.change(itemInput, { target: { value: "42" } });
+  expect(getByDisplayValue("42")).toBeInTheDocument();
 });

a84c616ca27487443197282e63ca491690f2973d
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-09-17 18:28:18 +0200

    Test Button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,2 +1,17 @@
 import React from "react";
+import { render } from "@testing-library/react";
 import Button from "./Button";
+
+test("Button renders text", () => {
+  const { getByRole } = render(<Button>coucou</Button>);
+  expect(getByRole("button", { name: "coucou" })).toBeInTheDocument();
+});
+
+test("Button calls its click callback", () => {
+  const testClickHander = jest.fn();
+  const { getByRole } = render(
+    <Button onClick={testClickHander}>Test Button</Button>
+  );
+  getByRole("button", { name: /Test Button/i }).click();
+  expect(testClickHander).toHaveBeenCalledTimes(1);
+});

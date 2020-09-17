7f98e80053e4fde8710f0521486c044c3fa1b163
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-09-17 14:22:09 +0200

    Add app header

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1 className="title">Sorting Algorithm Visualizer</h1>
       </header>
     </div>
   );

09aebec3e4314061b038025504cc2eda85a13316
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-09-19 13:29:40 +0200

    Improve test suite

--- src/components/ArrayInput/ArrayInput.test.tsx
+++ src/components/ArrayInput/ArrayInput.test.tsx
@@ -18,9 +18,43 @@ test("updating the item value ", () => {
   expect(getByDisplayValue("42")).toBeInTheDocument();
 });
 
-test("edit mode activation", () => {
+test("edit mode", () => {
+  const testEditHandler = jest.fn();
   const { getByDisplayValue } = render(
-    <ArrayInput editingIndex={1} currentList={[42, 21, 13]} />
+    <ArrayInput
+      editingIndex={1}
+      currentList={[42, 21, 13]}
+      onEdit={testEditHandler}
+    />
   );
-  expect(getByDisplayValue("21")).toBeInTheDocument();
+  const testInput = getByDisplayValue("21");
+  expect(testInput).toBeInTheDocument();
+  fireEvent.change(testInput, { target: { value: "105" } });
+  expect(testEditHandler).toHaveBeenCalledWith("105");
+});
+
+test("edit mode activation", () => {
+  const testOnEditStartHandler = jest.fn();
+  const { getByRole } = render(
+    <ArrayInput
+      currentList={[42, 21, 13]}
+      onEditStart={testOnEditStartHandler}
+    />
+  );
+  getByRole("button", { name: "21" }).click();
+  expect(testOnEditStartHandler).toHaveBeenCalledWith(1);
+});
+
+test("edit mode stop", () => {
+  const testEditEndHandler = jest.fn();
+  const { getByDisplayValue } = render(
+    <ArrayInput
+      currentList={[42, 21, 13]}
+      editingIndex={1}
+      onEditEnd={testEditEndHandler}
+    />
+  );
+  const testInput = getByDisplayValue("21");
+  fireEvent.blur(testInput);
+  expect(testEditEndHandler).toHaveBeenCalled();
 });
